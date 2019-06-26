a6c0b757ca1b5043cd7c1529b38cbde756125d55
AndreasRaisl <andreas.raisl@gmx.de>
2019-06-26 10:35:42 +0200

    better User Experience, add button with Event Listener

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 let Phrase = require("mhartl-palindrome");
 
-let userInput = prompt("Give me your string please: ");
-let phrase = new Phrase(userInput);
-console.log(phrase.content);
 
-if (phrase.palindrome()) alert(`"${phrase.content}" is a palindrome`);
-else alert(`"${phrase.content}" is not a palindrome`);
+
+function palindromeTester()
+{
+    let userInput = prompt("Give me your string please: ");
+    let phrase = new Phrase(userInput);
+    if (phrase.palindrome()) alert(`"${phrase.content}" is a palindrome`);
+    else alert(`"${phrase.content}" is not a palindrome`);
+
+}
+
+let button = document.querySelector("#palindromeTester");
+button.addEventListener("click", function() {
+  palindromeTester();
+});
